4cace7f2350d1dc4906fd36d531d7350f31cfd8c
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-02 15:14:14 +0100

    use recursive structure instead of separate variable for modifier in i3 config

--- modules/home-manager/i3/default.nix
+++ modules/home-manager/i3/default.nix
@@ -1,13 +1,12 @@
 { pkgs, lib, ... }:
 let
-  mod = "Mod4";
   screenshotLocation = "/home/florian/Pictures/Screenshots";
 in
 {
   xsession.windowManager.i3 = {
     enable = true;
-    config = {
-      modifier = mod;
+    config = rec {
+      modifier = "Mod4";
       gaps.inner = 8;
       window.titlebar = false;
 
@@ -27,54 +26,54 @@ in
 
       keybindings = {
         # Navigation
-        "${mod}+h" = "focus left";
-        "${mod}+j" = "focus down";
-        "${mod}+k" = "focus up";
-        "${mod}+l" = "focus right";
-        "${mod}+a" = "focus parent";
+        "${modifier}+h" = "focus left";
+        "${modifier}+j" = "focus down";
+        "${modifier}+k" = "focus up";
+        "${modifier}+l" = "focus right";
+        "${modifier}+a" = "focus parent";
 
-        "${mod}+Shift+h" = "move left";
-        "${mod}+Shift+j" = "move down";
-        "${mod}+Shift+k" = "move up";
-        "${mod}+Shift+l" = "move right";
+        "${modifier}+Shift+h" = "move left";
+        "${modifier}+Shift+j" = "move down";
+        "${modifier}+Shift+k" = "move up";
+        "${modifier}+Shift+l" = "move right";
 
         # Workspace
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
 
-        "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
-        "${mod}+Shift+3" = "move container to workspace number 3";
-        "${mod}+Shift+4" = "move container to workspace number 4";
-        "${mod}+Shift+5" = "move container to workspace number 5";
-        "${mod}+Shift+6" = "move container to workspace number 6";
-        "${mod}+Shift+7" = "move container to workspace number 7";
-        "${mod}+Shift+8" = "move container to workspace number 8";
-        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
 
         # Layout
-        "${mod}+Shift+v" = "split h";
-        "${mod}+v" = "split v";
-        "${mod}+s" = "layout stacking";
-        "${mod}+w" = "layout tabbed";
-        "${mod}+e" = "layout toggle split";
+        "${modifier}+Shift+v" = "split h";
+        "${modifier}+v" = "split v";
+        "${modifier}+s" = "layout stacking";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+e" = "layout toggle split";
 
-        "${mod}+f" = "fullscreen toggle";
+        "${modifier}+f" = "fullscreen toggle";
 
         # Scratchpad
-        "${mod}+minus" = "scratchpad show";
-        "${mod}+Shift+minus" = "move scratchpad";
+        "${modifier}+minus" = "scratchpad show";
+        "${modifier}+Shift+minus" = "move scratchpad";
 
         # Floating
-        "${mod}+space" = "focus mode_toggle";
-        "${mod}+Shift+space" = "floating toggle";
+        "${modifier}+space" = "focus mode_toggle";
+        "${modifier}+Shift+space" = "floating toggle";
 
         # Audio
         "XF86AudioRaiseVolume" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
@@ -86,20 +85,20 @@ in
         "XF86AudioPrev" = "exec --no-startup-id ${pkgs.playerctl}/bin/playerctl previous";
 
         # Screenshot
-        "${mod}+Shift+F3" = ''exec --no-startup-id "mkdir -p ${screenshotLocation};            ${pkgs.scrot}/bin/scrot -F '${screenshotLocation}/%Y-%m-%d_%H:%M:%s.jpg'   "'';
-        "${mod}+Shift+F4" = ''exec --no-startup-id "mkdir -p ${screenshotLocation}; sleep 0.2; ${pkgs.scrot}/bin/scrot -F '${screenshotLocation}/%Y-%m-%d_%H:%M:%s.jpg' -s"'';
-        "${mod}+Control+Shift+F3" = ''exec --no-startup-id "mkdir -p ${screenshotLocation};            ${pkgs.scrot}/bin/scrot -F '/tmp/scrot_%Y-%m-%d_%H:%M:%s.jpg' -o -e '${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i $f'   "'';
-        "${mod}+Control+Shift+F4" = ''exec --no-startup-id "mkdir -p ${screenshotLocation}; sleep 0.2; ${pkgs.scrot}/bin/scrot -F '/tmp/scrot_%Y-%m-%d_%H:%M:%s.jpg' -o -e '${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i $f' -s"'';
+        "${modifier}+Shift+F3" = ''exec --no-startup-id "mkdir -p ${screenshotLocation};            ${pkgs.scrot}/bin/scrot -F '${screenshotLocation}/%Y-%m-%d_%H:%M:%s.jpg'   "'';
+        "${modifier}+Shift+F4" = ''exec --no-startup-id "mkdir -p ${screenshotLocation}; sleep 0.2; ${pkgs.scrot}/bin/scrot -F '${screenshotLocation}/%Y-%m-%d_%H:%M:%s.jpg' -s"'';
+        "${modifier}+Control+Shift+F3" = ''exec --no-startup-id "mkdir -p ${screenshotLocation};            ${pkgs.scrot}/bin/scrot -F '/tmp/scrot_%Y-%m-%d_%H:%M:%s.jpg' -o -e '${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i $f'   "'';
+        "${modifier}+Control+Shift+F4" = ''exec --no-startup-id "mkdir -p ${screenshotLocation}; sleep 0.2; ${pkgs.scrot}/bin/scrot -F '/tmp/scrot_%Y-%m-%d_%H:%M:%s.jpg' -o -e '${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i $f' -s"'';
 
         # Other
-        "${mod}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
-        "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
-        "${mod}+Control+q" = "exec ${pkgs.systemd}/bin/loginctl lock-session";
+        "${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+        "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+        "${modifier}+Control+q" = "exec ${pkgs.systemd}/bin/loginctl lock-session";
 
-        "${mod}+r" = "mode resize";
-        "${mod}+Shift+q" = "kill";
-        "${mod}+Shift+c" = "reload";
-        "${mod}+Shift+r" = "restart";
+        "${modifier}+r" = "mode resize";
+        "${modifier}+Shift+q" = "kill";
+        "${modifier}+Shift+c" = "reload";
+        "${modifier}+Shift+r" = "restart";
       };
 
       keycodebindings = {
@@ -119,7 +118,7 @@ in
 
             Escape = "mode default";
             Return = "mode default";
-            "${mod}+r" = "mode default";
+            "${modifier}+r" = "mode default";
           };
       };
 
