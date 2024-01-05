c94d5cdba5bdb78423c282d56a255ce8a62a251f
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-05 10:30:41 +0100

    add dunst notification daemon to i3 config

--- modules/home-manager/i3/default.nix
+++ modules/home-manager/i3/default.nix
@@ -196,6 +196,114 @@ in
     };
   };
 
+  services.dunst = {
+    enable = true;
+
+    settings = {
+      global = {
+        ### Display ###
+
+        monitor = "0";
+        follow = "mouse";
+
+        ### Geometry ###
+
+        origin = "top-center";
+        offset = "0x50";
+        height = "300";
+
+        padding = "8";
+        horizontal_padding = "10";
+        text_icon_padding = "0";
+
+        ### Color ###
+
+        # transparency = "15";
+
+        separator_height = "1";
+        separator_color = "frame";
+
+        frame_width = "0";
+        frame_color = "#282a36";
+
+        sort = "yes";
+        idle_threshold = "120";
+
+        ### Text ###
+
+        font = "Monospace 10";
+        line_height = "0";
+
+        format = "<b>%a</b> %s %p\n%b";
+        markup = "full";
+
+        alignment = "left";
+        vertical_alignment = "center";
+
+        show_age_threshold = "60";
+        ellipsize = "middle";
+
+        ignore_newline = "no";
+
+        stack_duplicates = "true";
+        hide_duplicate_count = "false";
+        show_indicators = "yes";
+
+        ### Icons ###
+
+        icon_position = "left";
+        min_icon_size = "0";
+        max_icon_size = "64";
+
+        ### History ###
+
+        sticky_history = "yes";
+        history_length = "20";
+
+        ### Misc/Advanced ###
+
+        dmenu = "/usr/bin/dmenu -p dunst:";
+        browser = "/usr/bin/firefox -new-tab";
+
+        always_run_script = "true";
+
+        title = "Dunst";
+        class = "Dunst";
+
+        corner_radius = "0";
+
+        ignore_dbusclose = "false";
+
+        ### Mouse ###
+
+        mouse_left_click = "close_current";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
+      };
+
+      experimental.per_monitor_dpi = "false";
+
+      urgency_low = {
+        background = "#282a36";
+        foreground = "#6272a4";
+        timeout = "10";
+      };
+
+      urgency_normal = {
+        background = "#282a36";
+        foreground = "#bd93f9";
+        timeout = "10";
+      };
+
+      urgency_critical = {
+        background = "#ff5555";
+        foreground = "#f8f8f2";
+        frame_color = "#ff5555";
+        timeout = "0";
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     systemd
     i3status
