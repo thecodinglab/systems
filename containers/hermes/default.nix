32c01cacdb5164b02e6887cc6edd9db68161be2e
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-07 17:04:33 +0100

    restore ips when using cloudflare

--- containers/hermes/default.nix
+++ containers/hermes/default.nix
@@ -1,4 +1,4 @@
-args@{ lib, ... }:
+args@{ pkgs, lib, ... }:
 let
   config = args.hermes or { };
 
@@ -31,6 +31,28 @@ in
 {
   services.nginx = {
     enable = true;
+
+    commonHttpConfig =
+      let
+        realIPsFromList = lib.strings.concatMapStringsSep "\n" (x: "set_real_ip_from  ${x};");
+        fileToList = (x: lib.strings.splitString "\n" (builtins.readFile x));
+
+        cfipv4 = fileToList (pkgs.fetchurl {
+          url = "https://www.cloudflare.com/ips-v4";
+          sha256 = "0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
+        });
+
+        cfipv6 = fileToList (pkgs.fetchurl {
+          url = "https://www.cloudflare.com/ips-v6";
+          sha256 = "1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
+        });
+      in
+      ''
+        ${realIPsFromList cfipv4}
+        ${realIPsFromList cfipv6}
+        real_ip_header CF-Connecting-IP;
+      '';
+
 
     virtualHosts = vhosts;
   };
