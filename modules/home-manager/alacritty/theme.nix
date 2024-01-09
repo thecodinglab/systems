03ba71fa4e7c2a83c8ae610214372a9b6cfe7f72
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-09 13:14:21 +0100

    change alacritty theme to nord

--- modules/home-manager/alacritty/theme.nix
+++ modules/home-manager/alacritty/theme.nix
@@ -1,52 +1,56 @@
 {
+  # source: https://github.com/nordtheme/alacritty/blob/9949642f3903e8fcb62bfc03f09410e3d78440c2/src/nord.yaml
   primary = {
-    background = "0x191724";
-    foreground = "0xe0def4";
+    background = "#2e3440";
+    foreground = "#d8dee9";
+    dim_foreground = "#a5abb6";
   };
   cursor = {
-    text = "0xe0def4";
-    cursor = "0x524f67";
+    text = "#2e3440";
+    cursor = "#d8dee9";
   };
   vi_mode_cursor = {
-    text = "0xe0def4";
-    cursor = "0x524f67";
-  };
-  line_indicator = {
-    foreground = "None";
-    background = "None";
+    text = "#2e3440";
+    cursor = "#d8dee9";
   };
   selection = {
-    text = "0xe0def4";
-    background = "0x403d52";
+    text = "CellForeground";
+      background= "#4c566a";
+  };
+  search = {
+    matches = {
+      foreground = "CellBackground";
+        background= "#88c0d0";
+    };
   };
   normal = {
-    black = "0x26233a";
-    red = "0xeb6f92";
-    green = "0x31748f";
-    yellow = "0xf6c177";
-    blue = "0x9ccfd8";
-    magenta = "0xc4a7e7";
-    cyan = "0xebbcba";
-    white = "0xe0def4";
+    black = "#3b4252";
+    red = "#bf616a";
+    green = "#a3be8c";
+    yellow = "#ebcb8b";
+    blue = "#81a1c1";
+    magenta = "#b48ead";
+    cyan = "#88c0d0";
+    white = "#e5e9f0";
   };
   bright = {
-    black = "0x6e6a86";
-    red = "0xeb6f92";
-    green = "0x31748f";
-    yellow = "0xf6c177";
-    blue = "0x9ccfd8";
-    magenta = "0xc4a7e7";
-    cyan = "0xebbcba";
-    white = "0xe0def4";
+    black = "#4c566a";
+    red = "#bf616a";
+    green = "#a3be8c";
+    yellow = "#ebcb8b";
+    blue = "#81a1c1";
+    magenta = "#b48ead";
+    cyan = "#8fbcbb";
+    white = "#eceff4";
   };
-  hints = {
-    start = {
-      foreground = "#908caa";
-      background = "#1f1d2e";
-    };
-    end = {
-      foreground = "#6e6a86";
-      background = "#1f1d2e";
-    };
+  dim = {
+    black = "#373e4d";
+    red = "#94545d";
+    green = "#809575";
+    yellow = "#b29e75";
+    blue = "#68809a";
+    magenta = "#8c738c";
+    cyan = "#6d96a5";
+    white = "#aeb3bb";
   };
 }
