a7301f5c55eb2ea2520462033b5fb86831060b21
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-14 10:59:13 +0100

    add automatic garbage collector

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -3,9 +3,17 @@ let
   neovim = neovim-config.makeDistribution pkgs;
 in
 {
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
   };
 
   environment = {
