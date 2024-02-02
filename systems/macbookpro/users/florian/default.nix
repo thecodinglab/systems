f7c085f0e4a9fae5310225e7e00cc1da3b334050
Florian Walter <nairolf.retlaw@gmail.com>
2024-02-02 13:18:08 +0100

    add kitty configuration for macos

--- systems/macbookpro/users/florian/default.nix
+++ systems/macbookpro/users/florian/default.nix
@@ -20,7 +20,9 @@
             (root + "/modules/home-manager/alacritty")
             (root + "/modules/home-manager/fzf")
             (root + "/modules/home-manager/git")
+            (root + "/modules/home-manager/kitty")
             (root + "/modules/home-manager/lf")
+            (root + "/modules/home-manager/sioyek")
             (root + "/modules/home-manager/tmux")
             (root + "/modules/home-manager/zsh")
           ];
@@ -29,36 +31,44 @@
           # Customisations       #
           ########################
 
-          programs.alacritty.settings = {
-            window = {
-              decorations = "buttonless";
-              padding = { x = 4; y = 2; };
+          programs = {
+            alacritty.settings = {
+              window = {
+                decorations = "buttonless";
+                padding = { x = 4; y = 2; };
+              };
+
+              font.size = 14;
+
+              keyboard.bindings = [
+                {
+                  key = "N";
+                  mods = "Command|Shift";
+                  action = "CreateNewWindow";
+                }
+                {
+                  key = "N";
+                  mods = "Command|Control";
+                  action = "SpawnNewInstance";
+                }
+                {
+                  key = "Left";
+                  mods = "Alt";
+                  chars = "\\u001bb";
+                }
+                {
+                  key = "Right";
+                  mods = "Alt";
+                  chars = "\\u001bf";
+                }
+              ];
             };
 
-            font.size = 14;
-
-            keyboard.bindings = [
-              {
-                key = "N";
-                mods = "Command|Shift";
-                action = "CreateNewWindow";
-              }
-              {
-                key = "N";
-                mods = "Command|Control";
-                action = "SpawnNewInstance";
-              }
-              {
-                key = "Left";
-                mods = "Alt";
-                chars = "\\u001bb";
-              }
-              {
-                key = "Right";
-                mods = "Alt";
-                chars = "\\u001bf";
-              }
-            ];
+            kitty.settings = {
+              macos_show_window_title_in = "none";
+              hide_window_decorations = "titlebar-only";
+              window_padding_width = 1;
+            };
           };
         });
 
