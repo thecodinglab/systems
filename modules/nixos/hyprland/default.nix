490751e43d1a06de0d0458423ad02cdde4199874
Florian Walter <nairolf.retlaw@gmail.com>
2024-04-20 17:24:29 +0200

    fix system configuration for linux

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, hyprland, ... }: {
+{ inputs, pkgs, ... }: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = false;
 
-    package = hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 }
