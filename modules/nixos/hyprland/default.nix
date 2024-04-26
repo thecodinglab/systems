cc57297f91f1249eb002ca95c5ed542fa83b8177
Florian Walter <nairolf.retlaw@gmail.com>
2024-04-26 12:24:39 +0200

    add greetd service to automatically start hyprland

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -3,4 +3,14 @@
     enable = true;
     xwayland.enable = false;
   };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${lib.getExe pkgs.greetd.tuigreet} --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 }
