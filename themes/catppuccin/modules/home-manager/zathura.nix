20e6cdc706d39f54b28108c8fa29fad19da53fc7
Florian Walter <fw@florian-walter.ch>
2024-06-04 18:13:07 +0200

    add transparent background to zathura

--- themes/catppuccin/modules/home-manager/zathura.nix
+++ themes/catppuccin/modules/home-manager/zathura.nix
@@ -3,13 +3,13 @@ let
   sources = import ./npins;
   colors = (lib.importJSON "${sources.palette}/palette.json").${config.catppuccin.flavor}.colors;
 
-  makeRGBA = ({ r, g, b }: "rgba(${toString r}, ${toString g}, ${toString b}, 1)");
+  makeRGBA = ({ r, g, b, a }: "rgba(${toString r}, ${toString g}, ${toString b}, ${toString a})");
 in
 {
   programs.zathura = lib.mkIf config.catppuccin.enable {
     options = {
       recolor-keephue = true;
-      default-bg = makeRGBA colors.mantle.rgb;
+      default-bg = makeRGBA (colors.base.rgb // { a = 0.8; });
     };
 
     extraConfig = builtins.readFile
