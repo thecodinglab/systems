f7e6244570bf7fc5ffde0718c6802761983616e0
Florian Walter <fw@florian-walter.ch>
2024-07-21 12:21:35 +0200

    fix kitty configuration

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -19,22 +19,35 @@
         size = if pkgs.stdenv.isDarwin then 14 else 12;
       };
 
-      settings =
-        {
-          dynamic_background_opacity = true;
-          background_opacity = "0.8";
-          background_blur = 8;
+      settings = pkgs.lib.mkMerge [
+        (
+          let
+            background = pkgs.runCommand "wallpaper-blurred.png" { buildInputs = [ pkgs.ffmpeg ]; } ''
+              ffmpeg -y \
+                -i ${./wallpaper/wallpaper.jpg} \
+                -vf "gblur=sigma=30" \
+                $out
+            '';
+          in
+          {
+            dynamic_background_opacity = true;
 
-          clipboard_control = "write-clipboard write-primary read-clipboard-ask read-primary-ask";
+            background_image = "${background}";
+            background_image_layout = "cscaled";
+            background_tint = "0.8";
 
-          allow_remote_control = "socket-only";
-          listen_on = "unix:/tmp/kitty";
-        }
-        // lib.mkIf pkgs.stdenv.isDarwin {
+            clipboard_control = "write-clipboard write-primary read-clipboard-ask read-primary-ask";
+
+            allow_remote_control = "socket-only";
+            listen_on = "unix:/tmp/kitty";
+          }
+        )
+        (lib.mkIf pkgs.stdenv.isDarwin {
           macos_show_window_title_in = "none";
           hide_window_decorations = "titlebar-only";
           window_padding_width = 1;
-        };
+        })
+      ];
     };
   };
 }
