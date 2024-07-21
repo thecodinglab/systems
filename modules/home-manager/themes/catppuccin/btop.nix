24c2409e454262fde3a53861fe4082e361c0a1a3
Florian Walter <fw@florian-walter.ch>
2024-07-21 14:09:24 +0200

    improve btop theme config

--- modules/home-manager/themes/catppuccin/btop.nix
+++ modules/home-manager/themes/catppuccin/btop.nix
@@ -1,12 +1,7 @@
 { config, lib, ... }:
-let
-  sources = import ./npins;
-  enable = config.custom.catppuccin.enable && config.programs.btop.enable;
-in
 {
-  programs.btop.settings = lib.mkIf enable { color_theme = "catppuccin.theme"; };
-
-  xdg.configFile."btop/themes/catppuccin.theme" = lib.mkIf enable {
-    source = "${sources.btop}/themes/catppuccin_${config.custom.catppuccin.flavor}.theme";
+  config = lib.mkIf (config.custom.catppuccin.enable && config.programs.btop.enable) {
+    programs.btop.settings.color_theme = "catppuccin.theme";
+    xdg.configFile."btop/themes/catppuccin.theme".source = "${(import ./npins).btop}/themes/catppuccin_${config.custom.catppuccin.flavor}.theme";
   };
 }
