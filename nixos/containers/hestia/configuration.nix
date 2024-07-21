3e64063da4d5660d8389f9b33ef7bfec943de483
Florian Walter <fw@florian-walter.ch>
2024-07-21 17:25:50 +0200

    refactor hestia config

---
+++ nixos/containers/hestia/configuration.nix
@@ -0,0 +1,158 @@
+{ modulesPath, lib, ... }:
+{
+  system.stateVersion = "23.11";
+
+  imports = [ (modulesPath + "/virtualisation/lxc-container.nix") ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  networking = {
+    hostName = "hestia";
+    firewall.allowedTCPPorts = [
+      3000
+      4000
+      8123
+    ];
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = lib.splitString "\n" (
+    builtins.readFile (
+      builtins.fetchurl {
+        name = "ssh-authorized-keys-v1";
+        url = "https://github.com/thecodinglab.keys";
+        sha256 = "fobgOm3SyyClt8TM74PXjyM9JjbXrXJ52na7TjJdKA0=";
+      }
+    )
+  );
+
+  systemd.timers.podman-auto-update = {
+    timerConfig = {
+      Unit = "podman-auto-update.service";
+      OnCalendar = "Mon 02:00";
+      Persistent = true;
+    };
+    wantedBy = [ "timers.target" ];
+  };
+
+  virtualisation.oci-containers.containers = {
+    teslamate-core = {
+      hostname = "teslamate-core";
+      image = "docker.io/teslamate/teslamate:latest";
+      autoStart = true;
+
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+
+      dependsOn = [
+        "teslamate-postgres"
+        "teslamate-mqtt"
+        "teslamate-grafana"
+      ];
+
+      environment = {
+        # FIXME: the current encryption key contains a '\n' character, which is
+        # currently not supported by env-files in podman (see
+        # https://github.com/containers/podman/issues/18724), also it seems to
+        # be currently not possible to rotate teslamate keys and thus, this
+        # cannot be moved to sops right now.
+        ENCRYPTION_KEY = builtins.readFile ../../../secrets/teslamate-key.txt;
+        DATABASE_USER = "teslamate";
+        DATABASE_PASS = "teslamate";
+        DATABASE_NAME = "teslamate";
+        # FIXME: for some reason teslamate is unable to resolve the hostname
+        #        (podman issue?)
+        DATABASE_HOST = "10.88.0.4";
+        MQTT_HOST = "10.88.0.3";
+      };
+
+      ports = [ "4000:4000" ];
+    };
+
+    teslamate-postgres = {
+      hostname = "teslamate-postgres";
+      image = "docker.io/postgres:15";
+      autoStart = true;
+
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+
+      environment = {
+        POSTGRES_USER = "teslamate";
+        POSTGRES_PASSWORD = "teslamate";
+        POSTGRES_DB = "teslamate";
+      };
+      volumes = [ "teslamate-postgres-data:/var/lib/postgresql/data" ];
+
+      extraOptions = [ "--ip=10.88.0.4" ];
+    };
+
+    teslamate-grafana = {
+      hostname = "teslamate-grafana";
+      image = "docker.io/teslamate/grafana:latest";
+      autoStart = true;
+
+      dependsOn = [ "teslamate-postgres" ];
+
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+
+      environment = {
+        DATABASE_USER = "teslamate";
+        DATABASE_PASS = "teslamate";
+        DATABASE_NAME = "teslamate";
+        # FIXME: for some reason grafana is unable to resolve the hostname
+        #        (podman issue?)
+        DATABASE_HOST = "10.88.0.4";
+
+        GF_AUTH_ANONYMOUS_ENABLED = "true";
+        GF_SERVER_DOMAIN = "teslamate.thecodinglab.ch";
+        GF_SERVER_ROOT_URL = "%(protocol)s://%(domain)s/grafana";
+        GF_SERVER_SERVE_FROM_SUB_PATH = "true";
+      };
+      ports = [ "3000:3000" ];
+      volumes = [ "teslamate-grafana-data:/var/lib/grafana" ];
+    };
+
+    teslamate-mqtt = {
+      hostname = "teslamate-mqtt";
+      image = "docker.io/eclipse-mosquitto:2";
+      cmd = [
+        "mosquitto"
+        "-c"
+        "/mosquitto-no-auth.conf"
+      ];
+      autoStart = true;
+
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+
+      volumes = [
+        "teslamate-misquitto-config:/mosquitto/config"
+        "teslamate-misquitto-data:/mosquitto/data"
+      ];
+
+      extraOptions = [ "--ip=10.88.0.3" ];
+    };
+  };
+
+  services.home-assistant = {
+    enable = true;
+
+    extraComponents = [ "hue" ];
+
+    config = {
+      default_config = { };
+      http = {
+        use_x_forwarded_for = true;
+        trusted_proxies = "172.16.0.132";
+      };
+    };
+  };
+
+  # FIXME: see https://nixos.wiki/wiki/Home_Assistant
+  # nixpkgs.config.permittedInsecurePackages = [ "openssl-1.1.1w" ];
+}
---
+++ nixos/containers/poseidon/configuration.nix
@@ -0,0 +1,170 @@
+{ modulesPath, lib, ... }:
+let
+in
+{
+  system.stateVersion = "23.11";
+
+  imports = [ (modulesPath + "/virtualisation/lxc-container.nix") ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  networking = {
+    hostName = "poseidon";
+    firewall.allowedTCPPorts = [ 2049 ];
+  };
+
+  fileSystems = {
+    "/export/share" = {
+      device = "/mnt/share";
+      options = [ "bind" ];
+    };
+
+    "/export/timemachine" = {
+      device = "/mnt/timemachine";
+      options = [ "bind" ];
+    };
+
+    "/export/media/library" = {
+      device = "/mnt/media-library/libraries";
+      options = [ "bind" ];
+    };
+    "/export/media/downloads" = {
+      device = "/mnt/media-library/downloads/complete";
+      options = [ "bind" ];
+    };
+  };
+
+  users = {
+    users = {
+      root.openssh.authorizedKeys.keys = lib.splitString "\n" (
+        builtins.readFile (
+          builtins.fetchurl {
+            name = "ssh-authorized-keys-v1";
+            url = "https://github.com/thecodinglab.keys";
+            sha256 = "fobgOm3SyyClt8TM74PXjyM9JjbXrXJ52na7TjJdKA0=";
+          }
+        )
+      );
+
+      share = {
+        isSystemUser = true;
+        group = "share";
+      };
+
+      timemachine = {
+        isSystemUser = true;
+        group = "timemachine";
+      };
+
+      media = {
+        isSystemUser = true;
+        group = "media";
+        uid = 1000;
+      };
+    };
+
+    groups = {
+      share = { };
+      timemachine = { };
+      media = {
+        gid = 1000;
+      };
+    };
+  };
+
+  services = {
+    nfs.server = {
+      enable = true;
+      exports = ''
+        /export                 192.168.1.0/24(ro,crossmnt,fsid=0,no_subtree_check) 
+        /export/share           192.168.1.0/24(ro,nohide,insecure,no_subtree_check)
+        /export/media/library   192.168.1.0/24(ro,nohide,insecure,no_subtree_check)
+        /export/media/downloads 192.168.1.0/24(ro,nohide,insecure,no_subtree_check)
+      '';
+    };
+
+    samba = {
+      enable = true;
+      openFirewall = true;
+      securityType = "user";
+
+      extraConfig = ''
+        server string = poseidon
+        netbios name = poseidon
+        hosts allow = 192.168.1. 127.0.0.1 localhost
+        hosts deny = 0.0.0.0/0
+
+        multicast dns register = no
+
+        guest account = nobody
+      '';
+
+      shares = {
+        share = {
+          path = "/mnt/share";
+          browseable = "yes";
+          writable = "yes";
+          "valid users" = "share";
+          "force user" = "share";
+          "force group" = "share";
+        };
+
+        timemachine = {
+          path = "/mnt/timemachine";
+          browseable = "yes";
+          writable = "yes";
+          "valid users" = "timemachine";
+          "force user" = "timemachine";
+          "force group" = "timemachine";
+
+          "fruit:aapl" = "yes";
+          "fruit:time machine" = "yes";
+          "vfs objects" = "catia fruit streams_xattr";
+        };
+
+        media = {
+          path = "/mnt/media-library/libraries";
+          browseable = "yes";
+          writable = "no";
+          "guest ok" = "yes";
+          "force user" = "media";
+          "force group" = "media";
+        };
+      };
+    };
+
+    avahi = {
+      enable = true;
+      openFirewall = true;
+
+      publish = {
+        enable = true;
+        addresses = true;
+      };
+
+      extraServiceFiles = {
+        smb = ''
+          <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+          <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+          <service-group>
+            <name replace-wildcards="yes">%h</name>
+            <service>
+              <type>_smb._tcp</type>
+              <port>445</port>
+            </service>
+             <service>
+              <type>_device-info._tcp</type>
+              <port>0</port>
+              <txt-record>model=TimeCapsule8,119</txt-record>
+            </service>
+            <service>
+              <type>_adisk._tcp</type>
+              <txt-record>dk0=adVN=timemachine,adVF=0x82</txt-record>
+              <txt-record>sys=waMa=0,sys=adVF=0x100</txt-record>
+            </service>
+          </service-group>
+        '';
+      };
+    };
+  };
+}
