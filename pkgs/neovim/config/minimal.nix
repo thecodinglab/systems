e4c3925c2c9b7f90ae3877c324f1f978653b2835
Florian Walter <fw@florian-walter.ch>
2024-09-15 19:35:06 +0200

    reenable cursor animation

--- pkgs/neovim/config/minimal.nix
+++ pkgs/neovim/config/minimal.nix
@@ -10,7 +10,8 @@ in
   extraConfigLua = ''
     if vim.g.neovide then
       vim.g.neovide_hide_mouse_when_typing = true
-      vim.g.neovide_cursor_animation_length = 0
+      vim.g.neovide_cursor_animation_length = 0.1
+      vim.g.neovide_cursor_trail_size = 0.2
       vim.g.neovide_show_border = true
       vim.g.neovide_theme = 'auto'
 
