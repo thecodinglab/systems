4e66638a1787f66a00588668de8b24f18667a562
Florian Walter <fw@florian-walter.ch>
2024-10-08 09:18:06 +0200

    group nix-darwin defaults settings to one block

--- darwin/macbookpro/configuration.nix
+++ darwin/macbookpro/configuration.nix
@@ -26,97 +26,101 @@
     home = "/Users/florian/";
   };
 
-  system.defaults.NSGlobalDomain = {
-    AppleInterfaceStyleSwitchesAutomatically = true;
-    NSAutomaticCapitalizationEnabled = false;
-  };
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyleSwitchesAutomatically = true;
+      NSAutomaticCapitalizationEnabled = false;
+      KeyRepeat = 2;
+    };
 
-  system.defaults.finder = {
-    AppleShowAllExtensions = true;
-    FXPreferredViewStyle = "Nlsv";
-    FXDefaultSearchScope = "SCcf";
-  };
+    loginwindow.GuestEnabled = false;
 
-  system.defaults.WindowManager = {
-    # enable stage manager
-    GloballyEnabled = true;
-    # disable hiding of all applications when clicking on wallpaper
-    EnableStandardClickToShowDesktop = false;
+    finder = {
+      AppleShowAllExtensions = true;
+      FXPreferredViewStyle = "Nlsv";
+      FXDefaultSearchScope = "SCcf";
+    };
 
-    AppWindowGroupingBehavior = true;
-  };
+    WindowManager = {
+      # enable stage manager
+      GloballyEnabled = true;
+      # disable hiding of all applications when clicking on wallpaper
+      EnableStandardClickToShowDesktop = false;
 
-  system.defaults.dock = {
-    orientation = "bottom";
-    show-recents = false;
+      AppWindowGroupingBehavior = true;
+    };
 
-    autohide = true;
-    autohide-delay = 0.2;
+    dock = {
+      orientation = "bottom";
+      show-recents = false;
 
-    tilesize = 48;
-    magnification = true;
-    largesize = 64;
+      autohide = true;
+      autohide-delay = 0.2;
 
-    expose-group-by-app = true;
-    mru-spaces = false;
+      tilesize = 48;
+      magnification = true;
+      largesize = 64;
 
-    persistent-apps = [
-      "/Applications/Arc.app" # TODO: move into nix-darwin config
-      "/Applications/Safari.app"
-      "/System/Applications/Mail.app"
-      "/Applications/Slack.app" # TODO: move into nix-darwin config
-      "/System/Applications/Calendar.app"
+      expose-group-by-app = true;
+      mru-spaces = false;
 
-      "${pkgs.kitty}/Applications/kitty.app"
-      "${pkgs.neovide}/Applications/Neovide.app"
-      "${pkgs.obsidian}/Applications/Obsidian.app"
+      persistent-apps = [
+        "/Applications/Arc.app" # TODO: move into nix-darwin config
+        "/Applications/Safari.app"
+        "/System/Applications/Mail.app"
+        "/Applications/Slack.app" # TODO: move into nix-darwin config
+        "/System/Applications/Calendar.app"
 
-      "${pkgs.spotify}/Applications/Spotify.app"
-      "${pkgs._1password-gui}/Applications/1Password.app"
-      "/System/Applications/System Settings.app"
-      "/System/Applications/iPhone Mirroring.app"
+        "${pkgs.kitty}/Applications/kitty.app"
+        "${pkgs.obsidian}/Applications/Obsidian.app"
+
+        "${pkgs.spotify}/Applications/Spotify.app"
+        "${pkgs._1password-gui}/Applications/1Password.app"
+        "/System/Applications/System Settings.app"
+        "/System/Applications/iPhone Mirroring.app"
+      ];
+    };
+
+    CustomUserPreferences."com.apple.dock".persistent-others = [
+      {
+        tile-data = {
+          arrangement = 1; # 1 = name, 2 = date-added, 3 = date-modified, 4 = date-created, 5 = kind
+          displayas = 1; # 0 = stack, 1 = folder
+          showas = 2; # 0 = automatic, 1 = fan, 2 = grid, 3 = list
+
+          file-data = {
+            _CFURLString = "file:///Users/florian/Documents/";
+            _CFURLStringType = 15;
+          };
+        };
+        tile-type = "directory-tile";
+      }
+      {
+        tile-data = {
+          arrangement = 2; # 1 = name, 2 = date-added, 3 = date-modified, 4 = date-created, 5 = kind
+          displayas = 1; # 0 = stack, 1 = folder
+          showas = 1; # 0 = automatic, 1 = fan, 2 = grid, 3 = list
+
+          file-data = {
+            _CFURLString = "file:///Users/florian/Downloads/";
+            _CFURLStringType = 15;
+          };
+        };
+        tile-type = "directory-tile";
+      }
     ];
-  };
 
-  system.defaults.CustomUserPreferences."com.apple.dock".persistent-others = [
-    {
-      tile-data = {
-        arrangement = 1; # 1 = name, 2 = date-added, 3 = date-modified, 4 = date-created, 5 = kind
-        displayas = 1; # 0 = stack, 1 = folder
-        showas = 2; # 0 = automatic, 1 = fan, 2 = grid, 3 = list
+    menuExtraClock = {
+      ShowDate = 0;
+      ShowDayOfMonth = true;
+      ShowDayOfWeek = true;
 
-        file-data = {
-          _CFURLString = "file:///Users/florian/Documents/";
-          _CFURLStringType = 15;
-        };
-      };
-      tile-type = "directory-tile";
-    }
-    {
-      tile-data = {
-        arrangement = 2; # 1 = name, 2 = date-added, 3 = date-modified, 4 = date-created, 5 = kind
-        displayas = 1; # 0 = stack, 1 = folder
-        showas = 1; # 0 = automatic, 1 = fan, 2 = grid, 3 = list
+      Show24Hour = true;
+      ShowSeconds = true;
 
-        file-data = {
-          _CFURLString = "file:///Users/florian/Downloads/";
-          _CFURLStringType = 15;
-        };
-      };
-      tile-type = "directory-tile";
-    }
-  ];
-
-  system.defaults.menuExtraClock = {
-    ShowDate = 0;
-    ShowDayOfMonth = true;
-    ShowDayOfWeek = true;
-
-    Show24Hour = true;
-    ShowSeconds = true;
-
-    IsAnalog = false;
-    ShowAMPM = false;
+      IsAnalog = false;
+      ShowAMPM = false;
+    };
   };
 
   system.stateVersion = 5;
