59799a95fa553611ffdc318b39c44dc1bb17478a
Florian Walter <fw@florian-walter.ch>
2025-04-14 11:39:39 +0200

    neovim: keep cursor centered

--- pkgs/neovim/config/minimal.nix
+++ pkgs/neovim/config/minimal.nix
@@ -127,6 +127,28 @@
       mode = [ "n" ];
       action = "<cmd>vertical resize +10<cr>";
     }
+
+    # keep cursor centered
+    {
+      key = "j";
+      mode = [ "n" ];
+      action = "jzz";
+    }
+    {
+      key = "k";
+      mode = [ "n" ];
+      action = "kzz";
+    }
+    {
+      key = "<c-d>";
+      mode = [ "n" ];
+      action = "<c-d>zz";
+    }
+    {
+      key = "<c-u>";
+      mode = [ "n" ];
+      action = "<c-u>zz";
+    }
   ];
 
   colorschemes.catppuccin = {
