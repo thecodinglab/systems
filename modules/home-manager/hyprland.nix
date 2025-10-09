a634a4fc7a9724653202ac4603c4ab84757a2261
Florian Walter <fw@florian-walter.ch>
2025-10-09 10:06:02 +0200

    hyprland: disable annoying update and donation messages

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -144,6 +144,11 @@
             disable_splash_rendering = true;
           };
 
+          ecosystem = {
+            no_update_news = true;
+            no_donation_nag = true;
+          };
+
           #########################
           # Startup Programs      #
           #########################
